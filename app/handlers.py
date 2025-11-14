4deef3fbb7394d1b9cb7867d20157d395a547c17
= <akromjonrustamov56@gmail.com>
2025-11-15 00:01:33 +0500

    add other files

---
+++ app/handlers.py
@@ -0,0 +1,30 @@
+from aiogram import F, Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from .generate import ai_generate
+
+router = Router()
+
+
+class Gen(StatesGroup):
+    wait = State()
+
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Welcome, please write your promt.')
+
+
+@router.message(Gen.wait)
+async def stop_flood(message: Message):
+    await message.answer("Please wait, your promt generating...")
+
+
+@router.message()
+async def generating(message: Message, state: FSMContext):
+    await state.set_state(Gen.wait)
+    response = await ai_generate(message.text)
+    await message.answer(response)
+    await state.clear()
